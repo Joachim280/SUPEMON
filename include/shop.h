14229a9f824c2c4716e2c27e080378a7976c39d4
Matthewlcn <matthew.li-ching-ng@supinfo.com>
2025-02-05 19:11:02 +0400

    shop.h

---
+++ include/shop.h
@@ -0,0 +1,10 @@
+/* shop.h - Gestion de la boutique */
+#ifndef SHOP_H
+#define SHOP_H
+
+#include "player.h"
+
+// Ouvre la boutique pour acheter ou vendre des objets
+void enter_shop(Player *player);
+
+#endif
