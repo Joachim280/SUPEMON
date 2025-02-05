c82d0ee1d71e7cbf4838b6c76d31aa99a2764a86
Matthewlcn <matthew.li-ching-ng@supinfo.com>
2025-02-05 19:08:34 +0400

    center.h

---
+++ include/center.h
@@ -0,0 +1,10 @@
+/* center.h - Gestion du Supémon Center */
+#ifndef CENTER_H
+#define CENTER_H
+
+#include "player.h"
+
+// Soigne tous les Supémons du joueur gratuitement
+void heal_supemons(Player *player);
+
+#endif
